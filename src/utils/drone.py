c23519d429210903519a5ecc2c157893dc47dbf3
roel-remmerie <roel.remmerie@student.howest.be>
2026-04-30 16:49:13 +0200

    Improve documentation

--- src/utils/drone.py
+++ src/utils/drone.py
@@ -2,13 +2,13 @@ import time
 
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 
-FLIGHT_HEIGHT = 0.5
-GROUND_LEVEL = 0.0
+FLIGHT_HEIGHT = 0.5 # Height at which a drone will fly
+Z_MIN = 0.0 # The lowest height a drone can go (ground level)
 
-TAKEOFF_SECONDS = 2.0
-LANDING_SECONDS = 2.0
+T_TAKEOFF = 2.0 # time it takes a drone to go from Z_MIN to FLIGHT_HEIGHT
+T_LANDING = 2.0 # time it takes a drone to go from FLIGHT_HEIGHT to Z_MIN
 
-TIME_PER_METER = 4.0
+V_DRONE = 0.25 # the speed of a drone (in meters/second)
 
 class DroneCommand:
     @staticmethod
@@ -28,21 +28,21 @@ class DroneCommand:
     @staticmethod
     def take_off(scf: SyncCrazyflie):
         commander = scf.cf.high_level_commander
-        commander.takeoff(FLIGHT_HEIGHT, TAKEOFF_SECONDS)
-        time.sleep(TAKEOFF_SECONDS)
+        commander.takeoff(FLIGHT_HEIGHT, T_TAKEOFF)
+        time.sleep(T_TAKEOFF)
 
     @staticmethod
     def land(scf: SyncCrazyflie):
         commander = scf.cf.high_level_commander
-        commander.land(GROUND_LEVEL, LANDING_SECONDS)
-        time.sleep(LANDING_SECONDS)
+        commander.land(Z_MIN, T_LANDING)
+        time.sleep(T_LANDING)
         commander.stop()
 
     @staticmethod
-    def run_step(scf: SyncCrazyflie, move):
+    def run_step(scf: SyncCrazyflie, move: tuple[float, float, float, float]):
         x_p1, y_p1, delta_p1_p0, max_swarm_delta = move
-        duration = delta_p1_p0 * TIME_PER_METER
-        wait_time = max_swarm_delta * TIME_PER_METER
+        duration = delta_p1_p0 / V_DRONE
+        wait_time = max_swarm_delta / V_DRONE
 
         commander = scf.cf.high_level_commander
         commander.go_to(x_p1,y_p1,FLIGHT_HEIGHT,0,duration)
