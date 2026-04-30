5b6481af9ca01b2c6fb97b1ab413acd89f52b1ef
roel-remmerie <roel.remmerie@student.howest.be>
2026-04-30 16:39:03 +0200

    Update dronecommands to use absolute positioning

--- src/test/flight.py
+++ src/test/flight.py
@@ -10,13 +10,23 @@ import time
 
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 
-FLIGHT_HEIGHT = 1.0
+FLIGHT_HEIGHT = 0.5
 GROUND_LEVEL = 0.0
 
 TAKEOFF_SECONDS = 2.0
 LANDING_SECONDS = 2.0
 
-TIME_PER_METER = 4.0
+DURATION = 5.0
+
+SAFE_DISTANCE = 0.5
+
+MIN_X = SAFE_DISTANCE
+MIN_Y = SAFE_DISTANCE
+MIN_Z = SAFE_DISTANCE
+
+MAX_X = 4.95 - SAFE_DISTANCE
+MAX_Y = 3.15 - SAFE_DISTANCE
+MAX_Z = 2.50 - SAFE_DISTANCE
 
 class DroneCommand:
     @staticmethod
@@ -46,12 +56,43 @@ class DroneCommand:
         time.sleep(LANDING_SECONDS)
         commander.stop()
 
+    @staticmethod
+    def run_sequence(scf: SyncCrazyflie, sequence):
+        for move in sequence:
+            DroneCommand.run_step(scf, move)
+
+    @staticmethod
+    def run_step(scf: SyncCrazyflie, arguments):
+        x, y, z = arguments
+        commander = scf.cf.high_level_commander
+        commander.go_to(x,y,z,0,DURATION)
+        time.sleep(DURATION)
+
+moves = [[
+    (MIN_X, MIN_Y, MIN_Z),
+    (MIN_X, MAX_Y, MIN_Z),
+    (MIN_X, MAX_Y, MAX_Z),
+    (MIN_X, MIN_Y, MAX_Z),
+    (MAX_X, MIN_Y, MAX_Z),
+    (MAX_X, MAX_Y, MAX_Z),
+    (MAX_X, MAX_Y, MIN_Z),
+    (MAX_X, MIN_Y, MIN_Z),
+]]
+
 cflib.crtp.init_drivers()
 available = cflib.crtp.scan_interfaces()
 uris = [f"{aa[0]}/{RADIO_ADDRESS}" for aa in available if aa[0] != USB_DEFAULT_ADDRESS]
 uris = list(dict.fromkeys(uris))
 
+sequence_args = {uri: moves for uri in uris}
+
 factory = CachedCfFactory(rw_cache='./cache')
 with Swarm(uris, factory=factory) as swarm:
-    swarm.parallel_safe(DroneCommand.light_check)
-    swarm.parallel_safe(DroneCommand.take_off)
+
+    try:
+        swarm.parallel_safe(DroneCommand.light_check)
+        swarm.parallel_safe(DroneCommand.take_off)
+        swarm.parallel_safe(DroneCommand.run_sequence, args_dict=sequence_args)
+    finally:
+
+        swarm.parallel_safe(DroneCommand.land)
--- src/utils/drone.py
+++ src/utils/drone.py
@@ -2,7 +2,7 @@ import time
 
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 
-FLIGHT_HEIGHT = 1.0
+FLIGHT_HEIGHT = 0.5
 GROUND_LEVEL = 0.0
 
 TAKEOFF_SECONDS = 2.0
@@ -26,10 +26,6 @@ class DroneCommand:
         DroneCommand.deactivate_led_bit_mask(scf)
 
     @staticmethod
-    def arm(scf: SyncCrazyflie):
-        scf.cf.supervisor.send_arming_request(True)
-
-    @staticmethod
     def take_off(scf: SyncCrazyflie):
         commander = scf.cf.high_level_commander
         commander.takeoff(FLIGHT_HEIGHT, TAKEOFF_SECONDS)
@@ -43,42 +39,11 @@ class DroneCommand:
         commander.stop()
 
     @staticmethod
-    def run_square_sequence(scf: SyncCrazyflie):
-        box_size = 1.0
-        flight_time = 3.0
+    def run_step(scf: SyncCrazyflie, move):
+        x_p1, y_p1, delta_p1_p0, max_swarm_delta = move
+        duration = delta_p1_p0 * TIME_PER_METER
+        wait_time = max_swarm_delta * TIME_PER_METER
 
         commander = scf.cf.high_level_commander
-
-        commander.go_to(box_size, 0, 0, 0, flight_time, relative=True)
-        time.sleep(flight_time)
-
-        commander.go_to(0, box_size, 0, 0, flight_time, relative=True)
-        time.sleep(flight_time)
-
-        commander.go_to(-box_size, 0, 0, 0, flight_time, relative=True)
-        time.sleep(flight_time)
-
-        commander.go_to(0, -box_size, 0, 0, flight_time, relative=True)
-        time.sleep(flight_time)
-
-    @staticmethod
-    def run_sequence(scf: SyncCrazyflie, sequence):
-        cf = scf.cf
-
-        for arguments in sequence:
-            commander = scf.cf.high_level_commander
-
-            x, y, z = arguments[0], arguments[1], arguments[2]
-            duration = arguments[3]
-
-            print('Setting position {} to cf {}'.format((x, y, z), cf.link_uri))
-            commander.go_to(x, y, 0, 0, duration, relative=True)
-            time.sleep(duration)
-
-    @staticmethod
-    def run_step(scf: SyncCrazyflie, x, y, distance, max_distnce):
-        duration = distance * TIME_PER_METER
-        wait_time = max_distnce * TIME_PER_METER
-        commander = scf.cf.high_level_commander
-        commander.go_to(x,y,0,0,duration, relative=True)
+        commander.go_to(x_p1,y_p1,FLIGHT_HEIGHT,0,duration)
         time.sleep(wait_time)
